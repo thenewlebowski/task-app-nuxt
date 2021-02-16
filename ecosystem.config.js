bf2150f0a2d9fc943e4fa61356df52b4ed7eac1e
Coltonn <Cltnnielsen@gmail.com>
2021-02-16 15:15:33 -0700

    [Config] Modified PM2 config and added .example.env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'GOGrello',
+            name: 'gogrello',
             exec_mode: 'cluster',
             instances: 'max', // Or a number of instances
             script: './node_modules/nuxt/bin/nuxt.js',
