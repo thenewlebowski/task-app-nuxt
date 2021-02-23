c6486e9d0e457b136cfea4b5d49202ffc75cbf3b
Coltonn <Cltnnielsen@gmail.com>
2021-02-23 14:05:53 -0700

    [Lint] More lint fixes

--- components/TasksList.vue
+++ components/TasksList.vue
@@ -1,61 +1,59 @@
 <template>
-    <v-card>
-        <v-toolbar color="blue-grey">
-            <v-toolbar-title>
-                {{ title }}
-            </v-toolbar-title>
-            <v-spacer />
-            <v-text-field
-                v-model="search"
-                class="mr-4"
-                append-icon="mdi-magnify"
-                label="Search"
-                single-line
-                hide-details
-            />
-            <TaskForm />
-        </v-toolbar>
-        <v-data-table
-            :loading="loading"
-            :headers="headers"
-            :items="tasks"
-            :search="search"
-            :sort-by="['site', 'priority']"
-            :sort-desc="[false, true]"
-            :expanded.sync="expanded"
-            item-key="_id"
-            loading-text="Loading tasks... Please wait"
-            multi-sort
-            single-expand
-            show-expand
-            class="elevation-1"
-        >
-            <template v-slot:item.title="{ item }">
-                <div class="limit-characters">{{ item.title }}</div>
-            </template>
-            <template v-slot:expanded-item="{ headers, item }">
-                <td :colspan="headers.length" class="pb-3 wrap-hack">
-                    <p class="description">{{ item.description }}</p>
-                    <TaskForm :task-to-edit="item" />
-                    <ArchiveTaskModal
-                        :task-id="item._id"
-                        @deleted="
-                            tasks = $store.getters['tasks/getUnassignedTasks']
-                        "
-                    />
-                </td>
-            </template>
-            <template v-slot:item.reporter="{ item }">
-                <div>{{ getReporterName(item.reporter) || 'Unknown' }}</div>
-            </template>
-            <template v-slot:item.dateAdded="{ item }">
-                <div>{{ formattedDate(item.dateAdded) || 'N/A' }}</div>
-            </template>
-            <template v-slot:item.action="{ item }">
-                <slot :task="item" name="action" />
-            </template>
-        </v-data-table>
-    </v-card>
+  <v-card>
+    <v-toolbar color="blue-grey">
+      <v-toolbar-title>
+        {{ title }}
+      </v-toolbar-title>
+      <v-spacer />
+      <v-text-field
+        v-model="search"
+        class="mr-4"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      />
+      <TaskForm />
+    </v-toolbar>
+    <v-data-table
+      :loading="loading"
+      :headers="headers"
+      :items="tasks"
+      :search="search"
+      :sort-by="['site', 'priority']"
+      :sort-desc="[false, true]"
+      :expanded.sync="expanded"
+      item-key="_id"
+      loading-text="Loading tasks... Please wait"
+      multi-sort
+      single-expand
+      show-expand
+      class="elevation-1"
+    >
+      <template v-slot:item.title="{ item }">
+        <div class="limit-characters">{{ item.title }}</div>
+      </template>
+      <template v-slot:expanded-item="{ headers, item }">
+        <td :colspan="headers.length" class="pb-3 wrap-hack">
+          <p class="description">{{ item.description }}</p>
+          <TaskForm :task-to-edit="item" />
+          <ArchiveTaskModal
+            :task-id="item._id"
+            @deleted="tasks = $store.getters['tasks/getUnassignedTasks']"
+          />
+        </td>
+      </template>
+      <template v-slot:item.reporter="{ item }">
+        <div>{{ getReporterName(item.reporter) || 'Unknown' }}</div>
+      </template>
+      <template v-slot:item.dateAdded="{ item }">
+        <div>{{ formattedDate(item.dateAdded) || 'N/A' }}</div>
+      </template>
+      <template v-slot:item.action="{ item }">
+        <slot :task="item" name="action" />
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
@@ -64,72 +62,72 @@ import TaskForm from './TaskForm'
 import ArchiveTaskModal from './ArchiveTaskModal'
 
 export default {
-    components: {
-        TaskForm,
-        ArchiveTaskModal
+  components: {
+    TaskForm,
+    ArchiveTaskModal
+  },
+  mixins: [formValidatorMixin],
+  props: {
+    tasks: {
+      type: Array,
+      required: true
     },
-    mixins: [formValidatorMixin],
-    props: {
-        tasks: {
-            type: Array,
-            required: true
-        },
-        title: {
-            type: String,
-            default: 'Tasks'
-        }
-    },
-    data() {
-        return {
-            search: '',
-            expanded: [],
-            singleExpand: true,
-            loading: true,
-            headers: [
-                { text: 'Title', value: 'title' },
-                { text: 'Site', value: 'site' },
-                { text: 'Priority', value: 'priority' },
-                { text: 'Type', value: 'type' },
-                { text: 'Reporter', value: 'reporter' },
-                { text: 'Date', value: 'dateAdded', width: '120px' },
-                { text: 'Take', value: 'action', sortable: false }
-                // Todo: add points
-            ]
-        }
-    },
-    created() {
-        this.loading = false
-    },
-    methods: {
-        // TODO: clean data to enforce date objects
-        formattedDate(date) {
-            if (date === null) return 'N/A'
-            if (typeof date === 'string') {
-                return date.split('T')[0]
-            } else if (typeof date === 'object') {
-                return date.getMonth()
-            } else {
-                return 'N/A'
-            }
-        },
-        getReporterName(id) {
-            return this.$store.getters['user/getUsersNameAndIdKey'][id]
-        }
+    title: {
+      type: String,
+      default: 'Tasks'
     }
+  },
+  data() {
+    return {
+      search: '',
+      expanded: [],
+      singleExpand: true,
+      loading: true,
+      headers: [
+        { text: 'Title', value: 'title' },
+        { text: 'Site', value: 'site' },
+        { text: 'Priority', value: 'priority' },
+        { text: 'Type', value: 'type' },
+        { text: 'Reporter', value: 'reporter' },
+        { text: 'Date', value: 'dateAdded', width: '120px' },
+        { text: 'Take', value: 'action', sortable: false }
+        // Todo: add points
+      ]
+    }
+  },
+  created() {
+    this.loading = false
+  },
+  methods: {
+    // TODO: clean data to enforce date objects
+    formattedDate(date) {
+      if (date === null) return 'N/A'
+      if (typeof date === 'string') {
+        return date.split('T')[0]
+      } else if (typeof date === 'object') {
+        return date.getMonth()
+      } else {
+        return 'N/A'
+      }
+    },
+    getReporterName(id) {
+      return this.$store.getters['user/getUsersNameAndIdKey'][id]
+    }
+  }
 }
 </script>
 
 <style>
 .limit-characters {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    max-width: 200px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 200px;
 }
 .wrap-hack {
-    max-width: 1px;
+  max-width: 1px;
 }
 .description {
-    overflow-wrap: break-word;
+  overflow-wrap: break-word;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -6,135 +6,147 @@ require('dotenv').config()
 // const serveStatic = require('serve-static')
 
 module.exports = {
-    // serverMiddleware: [
-    //   '~/api/index.js'
-    // Will register redirect-ssl npm package
-    // 'redirect-ssl',
+  // serverMiddleware: [
+  //   '~/api/index.js'
+  // Will register redirect-ssl npm package
+  // 'redirect-ssl',
 
-    // Will register file from project api directory to handle /api/* requires
-    // { path: '/api', handler: '~/server/index.js' },
+  // Will register file from project api directory to handle /api/* requires
+  // { path: '/api', handler: '~/server/index.js' },
 
-    // We can create custom instances too
-    // { path: '/static2', handler: serveStatic(__dirname + '/static2') }
-    // ],
-    /*
-     ** Headers of the page
-     */
-    head: {
-        titleTemplate: '%s - ' + process.env.APP_NAME,
-        title: 'Gogrello',
-        meta: [
-            { charset: 'utf-8' },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content: process.env.npm_package_description || ''
-            }
-        ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-    },
-    /*
-     ** Customize the progress-bar color
-     */
-    loading: { color: '#fff' },
-    /*
-     ** Global CSS
-     */
-    css: [],
-    /*
-     ** Plugins to load before mounting the App
-     */
-    plugins: [
-        { src: '@/plugins/vue-notifications', ssr: false },
-        { src: '@/plugins/vuetify' }
-        // TODO: use moment?
-        // { src: '@/plugins/vue-moment' }
+  // We can create custom instances too
+  // { path: '/static2', handler: serveStatic(__dirname + '/static2') }
+  // ],
+  /*
+   ** Headers of the page
+   */
+  head: {
+    titleTemplate: '%s - ' + process.env.APP_NAME,
+    title: 'Gogrello',
+    meta: [
+      { charset: 'utf-8' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    /*
-     ** Nuxt.js dev-modules
-     */
-    buildModules: [
-        // Doc: https://github.com/nuxt-community/eslint-module
-        '@nuxtjs/eslint-module'
-    ],
-    /*
-     ** Nuxt.js modules
-     */
-    modules: [
-        // Doc: https://axios.nuxtjs.org/usage
-        '@nuxtjs/axios',
-        '@nuxtjs/auth'
-        // '@nuxtjs/proxy'
-    ],
-    auth: {
-        // plugins: ['~/plugins/auth.js'],
-        strategies: {
-            local: {
-                endpoints: {
-                    login: {
-                        url: '/api/auth/login',
-                        method: 'post'
-                        // propertyName: false
-                    },
-                    logout: {
-                        url: '/api/auth/logout',
-                        method: 'post'
-                    },
-                    user: {
-                        url: '/api/auth/user',
-                        method: 'get'
-                        // propertyName: 'user'
-                    }
-                }
-                // These are the defaults
-                // tokenType: false, // 'bearer'
-                // tokenRequired: false
-                // tokenName: 'authorization'
-            }
-        },
-        redirect: {
-            login: '/login',
-            logout: '/login',
-            callback: '/login',
-            home: '/'
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    { src: '@/plugins/vue-notifications', ssr: false },
+    { src: '@/plugins/vuetify' }
+    // TODO: use moment?
+    // { src: '@/plugins/vue-moment' }
+  ],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/auth'
+    // '@nuxtjs/proxy'
+  ],
+  auth: {
+    // plugins: ['~/plugins/auth.js'],
+    strategies: {
+      local: {
+        endpoints: {
+          login: {
+            url: '/api/auth/login',
+            method: 'post'
+            // propertyName: false
+          },
+          logout: {
+            url: '/api/auth/logout',
+            method: 'post'
+          },
+          user: {
+            url: '/api/auth/user',
+            method: 'get'
+            // propertyName: 'user'
+          }
         }
+        // These are the defaults
+        // tokenType: false, // 'bearer'
+        // tokenRequired: false
+        // tokenName: 'authorization'
+      }
     },
-    router: {
-        middleware: ['auth']
-    },
-    /*
-     ** Axios module configuration
-     ** See https://axios.nuxtjs.org/options
-     */
-    // env: {
-    //     sessionSecret: process.env.SECRET || 'prettydumbsecret'
-    // },
-    axios: {
-        baseURL: process.env.API_URL,
-        // prefix: '/api',
-        credentials: true,
-        // redirectError: {
-        //     401: '/login',
-        //     404: '/notfound'
-        // },
-        debug: true,
-        proxy: true
-        // },
-        // proxy: {
-        //     '/api': 'http://localhost:3000'
-    },
-    serverMiddleware: ['~/server'],
-    /*
-     ** Build configuration
-     */
-    build: {
-        /*
-         ** You can extend webpack config here
-         */
-        extend(config, ctx) {}
+    redirect: {
+      login: '/login',
+      logout: '/login',
+      callback: '/login',
+      home: '/'
     }
+  },
+  router: {
+    middleware: ['auth']
+  },
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  // env: {
+  //     sessionSecret: process.env.SECRET || 'prettydumbsecret'
+  // },
+  axios: {
+    baseURL: process.env.API_URL,
+    // prefix: '/api',
+    credentials: true,
+    // redirectError: {
+    //     401: '/login',
+    //     404: '/notfound'
+    // },
+    debug: true,
+    proxy: true
+    // },
+    // proxy: {
+    //     '/api': 'http://localhost:3000'
+  },
+  serverMiddleware: ['~/server'],
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
+  }
 }
